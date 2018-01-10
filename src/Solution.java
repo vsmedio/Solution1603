3296949aec889d83dea7bb9ddd616e78e58e0851
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 00:51:15 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Список и нити
+В методе main добавить в статический объект list 5 нитей. Каждая нить должна быть новым объектом класса Thread, работающим со своим объектом класса SpecialThread.
+
+
+Требования:
+1. В методе main создай 5 объектов типа SpecialThread.
+2. В методе main создай 5 объектов типа Thread.
+3. Добавь 5 разных нитей в список list.
+4. Каждая нить из списка list должна работать со своим объектом класса SpecialThread.
+5. Метод run класса SpecialThread должен выводить "it's a run method inside SpecialThread".
+
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) {
+        //Add your code here - добавьте свой код тут
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) {
+        //Add your code here - добавьте свой код тут
+        SpecialThread specialThread1 = new SpecialThread();
+        SpecialThread specialThread2 = new SpecialThread();
+        SpecialThread specialThread3 = new SpecialThread();
+        SpecialThread specialThread4 = new SpecialThread();
+        SpecialThread specialThread5 = new SpecialThread();
+
+        list.add(new Thread(specialThread1));
+        list.add(new Thread(specialThread2));
+        list.add(new Thread(specialThread3));
+        list.add(new Thread(specialThread4));
+        list.add(new Thread(specialThread5));
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
